b617746d759b4c2b57c1ee856ba2ab7115249117
Andrew Mueller <iglinskia44@gmail.com>
2023-09-06 19:50:01 -0500

    qf

--- data/development/NFL_Arenas.ts
+++ data/development/NFL_Arenas.ts
@@ -1,29 +1,29 @@
-import { NFLTeamName } from '@prisma/client'
+// import { NFLTeamName } from '@prisma/client'
 
 
-interface ArenaInitialDataType {
-    name: string
-    location: {
-        city: string
-        state: string
-        zip: number
-    }
-    homeTeams: NFLTeamName[]
-}
+// interface ArenaInitialDataType {
+//     name: string
+//     location: {
+//         city: string
+//         state: string
+//         zip: number
+//     }
+//     homeTeams: NFLTeamName[]
+// }
 
 
-const data: ArenaInitialDataType[] = []
+// const data: ArenaInitialDataType[] = []
 
-data.push({
-    name: "Lambeau",
-    homeTeams: ["Packers"],
-    location: {
-        city: "Green Bay",
-        state: "Wisconsin",
-        zip: 90210
-    }
-})
+// data.push({
+//     name: "Lambeau",
+//     homeTeams: ["Packers"],
+//     location: {
+//         city: "Green Bay",
+//         state: "Wisconsin",
+//         zip: 90210
+//     }
+// })
 
-// data.push({...})
-// data.push({...})
-// data.push({...})
+// // data.push({...})
+// // data.push({...})
+// // data.push({...})
