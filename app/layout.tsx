9a719c6df0030c221359d2df94fb078883670919
Andrew Mueller <iglinskia44@gmail.com>
2023-09-08 12:30:39 -0500

    qf

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,13 @@ import clsx from 'clsx'
 import '#/styles/global.css'
 import type { Metadata } from 'next'
 import { cookies } from 'next/headers'
-import Script from 'next/script'
 import { Toaster } from '#/components/ui/toaster'
 import { Lexend_Giga } from 'next/font/google'
-import { Providers } from './providers'
 const lexand = Lexend_Giga({
     subsets: ['latin'],
     variable: "--font-lexand"
 })
 
-console.log("lexand: ", lexand)
-/* className={inter.className} */
 
 export const metadata: Metadata = {
     title: 'Safe Stub',
@@ -30,7 +26,7 @@ const RootLayout = async ({
     return (
         <html lang="en"
             /* className={clsx("group/html", darkMode && "dark")} */
-            className={clsx("dark", lexand.variable)}
+            className={clsx("group/html", "dark", lexand.variable)}
             suppressHydrationWarning
         >
             <body className={"group/body"}>
