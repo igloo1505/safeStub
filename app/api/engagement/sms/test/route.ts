842d2e6a587b8c2b9e2f3799406f799b97a61903
Andrew Mueller <iglinskia44@gmail.com>
2023-10-12 17:25:34 -0500

    Son'of'a biscuit

--- app/api/engagement/sms/test/route.ts
+++ app/api/engagement/sms/test/route.ts
@@ -27,5 +27,5 @@ router
 
 
 export async function POST(request: NextRequest, ctx: RequestContext) {
-    return router.run(request, ctx);
+    return router.run(request, ctx) as Promise<NextResponse>
 }
