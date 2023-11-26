b9f5384ce2ec27dce0289718b531860f944170ba
safestub <admin@safestub.com>
2023-11-26 15:37:33 -0600

    Removing crawl from git.

--- app/api/webhooks/crawlTicketData/route.ts
+++ app/api/webhooks/crawlTicketData/route.ts
@@ -2,33 +2,33 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { createEdgeRouter } from "next-connect";
 import { prisma } from "#/db/db";
-import { VividSeats } from "#/classes/crawl/vividSeats/vividSeats";
-import { SeatGeek } from "#/classes/crawl/seatGeek/seatGeek";
+// import { VividSeats } from "#/classes/crawl/vividSeats/vividSeats";
 
+// import { SeatGeek } from "#/classes/crawl/seatGeek/seatGeek";
 interface RequestContext {
 }
 
 const router = createEdgeRouter<NextRequest, RequestContext>();
 
-const crawlVividSeats = async (headless: boolean) => {
-    let vividSeats = new VividSeats()
-    await vividSeats.init({ headless })
-    await vividSeats.crawl()
-    await vividSeats.close()
-    return vividSeats.ticketData
-}
+// const crawlVividSeats = async (headless: boolean) => {
+//     let vividSeats = new VividSeats()
+//     await vividSeats.init({ headless })
+//     await vividSeats.crawl()
+//     await vividSeats.close()
+//     return vividSeats.ticketData
+// }
 
-const crawlSeatGeek = async (headless: boolean) => {
-    let seatGeek = new SeatGeek()
-    await seatGeek.init({ headless })
-    await seatGeek.crawl()
-}
+// const crawlSeatGeek = async (headless: boolean) => {
+//     let seatGeek = new SeatGeek()
+//     await seatGeek.init({ headless })
+//     await seatGeek.crawl()
+// }
 
 router
     .get(async (req, ctx) => {
         try {
-            let vivid = await crawlVividSeats(false)
-            console.log("vivid: ", vivid)
+            // let vivid = await crawlVividSeats(false)
+            // console.log("vivid: ", vivid)
             // let seatGeek = await crawlSeatGeek(false)
             return new NextResponse(JSON.stringify({ success: true }));
         } catch (err) {
